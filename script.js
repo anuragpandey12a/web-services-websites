50797626892d399e84c3d95f5b9a49c5af83807d
anuragpandey12a <opanurag924@gmail.com>
2026-01-06 18:45:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// Reveal animation
+// ===== REVEAL ANIMATION =====
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
   reveals.forEach(r => {
     const windowHeight = window.innerHeight;
     const elementTop = r.getBoundingClientRect().top;
-    if (elementTop < windowHeight - 100) {
+    if(elementTop < windowHeight - 100){
       r.classList.add("active");
     }
   });
 });
 
-// Email protection
+// ===== EMAIL PROTECTION =====
 const emailUser = "emailopanurag924";
 const emailDomain = "gmail.com";
 const emailLink = document.getElementById("emailLink");
 
 emailLink.href = `mailto:${emailUser}@${emailDomain}`;
 emailLink.textContent = `${emailUser}@${emailDomain}`;
+
 
